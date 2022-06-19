bb24fb1e810f4133faed13c888d4e5fd67db225a
AndreiYa <36473307+AndreiYa@users.noreply.github.com>
2022-06-19 21:19:09 +0300

    add multi mode

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
-# Simple-CRUD-API
+GET /api/users
+POST /api/users
+GET /api/users/id
+PUT /api/users/id
+DELETE /api/users/id
+
+For example :
+http://localhost:3000/api/users = return all users
+
+How to
+npm install
+
+Run
+
+Run in dev =
+npm run dev
+
+Run in prod =
+npm run prod
+
+Run multi =
+npm run multi
--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,20 @@
+import dotenv from "dotenv";
 import http from "http";
 import {getUsers, addUser, notFound, getUserById, updateUser, deleteUser} from "./controller";
 import {createDatabase} from './helpers/fileHelper';
+import { cpus } from "node:os";
+import cluster from "node:cluster";
+
+dotenv.config({ path: __dirname+'/.env'});
+const PORT = process.env.PORT || 6000;
+const MULTI = process.env.MULTI;
+
 const db = async () => {
   await createDatabase();
 }
-db().then();
 
+db().then(() => {
   const server = http.createServer((req, res) => {
-
     if (req.method == "GET" && req.url === "/api/users") {
       return getUsers(req, res);
     } else if (req.method === 'GET' && req.url?.match(/\/api\/users\/(\w+)/)) {
@@ -22,9 +29,25 @@ db().then();
       return notFound(req, res);
     }
   });
-  server.listen(3000, () => {
-    console.log("Server is running on port 3000");
-  });
+
+  if (MULTI === 'true') {
+    if (cluster.isPrimary) {
+      for (let i = 0; i < cpus().length; i++) {
+        cluster.fork();
+      }
+      cluster.on('exit', (worker, code) => {
+        console.log(`Worker ${worker.id} finish work. Exit with code: ${code}`);
+        server.listen(process.env.PORT, () => console.log(`Worker ${cluster.worker?.id} started`));
+      });
+    } else {
+      server.listen(process.env.PORT, () => console.log(`Worker ${cluster.worker?.id} started`));
+    }
+  } else {
+    server.listen(process.env.PORT, () => console.log(`Server is running on port ${PORT}`));
+  }
+});
+
+
 
 
 
